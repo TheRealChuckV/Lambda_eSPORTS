83fde89c0da9cca2cc16aa71bd43c9d1c6dce4ba
MarcoGPR <godinopaipal@gmail.com>
2025-02-13 16:32:32 +0100

    Creazione Game(model,ctr,repository)

---
+++ Lambda_eSPORTS/src/main/java/com/example/demo/model/Game.java
@@ -0,0 +1,65 @@
+package com.example.demo.model;
+
+import org.springframework.data.annotation.Id;
+
+public class Game {
+	@Id
+	private int id;
+	private String name;
+	private String description;
+	private String image;
+
+	public Game(int id, String name, String description, String image) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.image = image;
+	}
+
+	public Game() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String imagine) {
+		this.image = imagine;
+}
+
+	@Override
+	public String toString() {
+		return "Game [id=" + id + ", name=" + name + ", description=" + description + ", imagine=" + image + "]";
+	}
+	
+	
+	
+}
+
+
